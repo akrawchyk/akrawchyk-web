b98c58fa05114b363a3c4b15a906405c47011a3f
Andrew Krawchyk <903716+akrawchyk@users.noreply.github.com>
2019-07-20 12:23:24 -0400

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,28 @@
+const inclusiveLanguage = require('@11ty/eleventy-plugin-inclusive-language');
+
+const postcss = require('postcss');
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(inclusiveLanguage);
+
+  eleventyConfig.addFilter('postcss', function(css) {
+    const plugins = [
+      require('postcss-import'),
+      require('colorguard'),
+      require('postcss-preset-env'),
+      require('postcss-flexbugs-fixes')
+    ];
+
+    return postcss(plugins).process(css)
+      .then(function(result) {
+        return result.code
+      })
+      .catch(function(error) {
+        console.log(error)
+      });
+  });
+
+  return {
+    htmlTemplateEngine: 'njk'
+  };
+};
---
+++ src/resume.md
@@ -0,0 +1,18 @@
+# Resume
+
+The three E's
+
+## Experience
+
+* Optoro
+* iStrategyLabs
+* General Assembly
+* General Dynamics
+
+## Extras
+
+* Vue Conf 2019
+
+## Education
+
+* Denison University
